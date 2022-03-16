0774259b697ba27e3b73383933d6000eabe96b6a
Adesh-04 <AdeshGadekar98@gmail.com>
2022-03-16 19:43:03 +0530

    Removed Unnesessary

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-
-import { Header, PropsHeader } from './components/header';
-import { Home, PropsHome } from './components/home';
-import './stylesheet/App.css';
+import './stylesheet/Style.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home name={'mohit'} age={22} />
-      <PropsHome name={'sam'} age={20}>
-        <p>This is props.children </p>
-      </PropsHome>
-      <PropsHeader name={'sam'} age={20}>
-        <p>This is class</p>
-      </PropsHeader>
+     
     </div>
   );
 }
