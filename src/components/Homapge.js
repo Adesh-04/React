f2d5a3f605a1cd76edf03875ab1fa88ac79789d6
Adesh-04 <AdeshGadekar98@gmail.com>
2022-03-16 21:16:41 +0530

    Adding header

---
+++ src/components/Homapge.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Header =()=>{
+
+
+    return(
+        <div className='header'>
+            <img alt="*/logo" />
+            <div>
+                
+            </div>
+
+        </div>
+    )
+}
