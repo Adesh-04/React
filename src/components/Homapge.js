eadfc36891c30949522d30707b700b6e0ec95935
Adesh-04 <AdeshGadekar98@gmail.com>
2022-03-18 15:46:37 +0530

    mern backend v0.1

--- src/components/Homapge.js
+++ src/components/Homapge.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown, Image } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 export const Header =()=>{
 
@@ -7,31 +7,11 @@ export const Header =()=>{
     return(
         <div className='header'>            
             
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Image alt="*/" >
-
-                </Image>
-                <Container>
-                <Navbar.Brand >React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse >
-                    <Nav className="me-auto">
-                    <Nav.Link href="#features">Products</Nav.Link>
-                    <Nav.Link href="#pricing">Cart</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                    </Nav>
-                    <Nav>
-                    <Nav.Link href="#deets">Login</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <form>
+                <label>Name:  </label> <input type='text' placeholder='enter' name='name' />
+                <label>Age:  </label> <input type='number' placeholder='enter' name='age' />
+                <label>email:  </label> <input type='text' placeholder='enter' name='email' />
+            </form>
 
         </div>
     )
