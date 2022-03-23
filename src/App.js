77be7daeca5bb947a5bca4c10ced01376bbf862b
devashish <devashishkukade261@gmail.com>
2022-03-23 16:29:51 +0530

    Routes part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import HomePage from './components/HomePage';
 import './stylesheet/Style.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App container">
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
